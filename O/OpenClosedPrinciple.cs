82f3c0eea50d536c730ea8ded44f101ee84b29f9
Marko Kostic <52660186+TheKleee@users.noreply.github.com>
2022-05-13 12:38:00 +0200

    Update OpenClosedPrinciple.cs

--- O/OpenClosedPrinciple.cs
+++ O/OpenClosedPrinciple.cs
@@ -14,7 +14,7 @@ public class OpenClosedPrinciple
         circle.SetShape(12);
         Shapes<string>[] shapes = new Shapes<string>[] { rect, circle };
         area.SetShape(shapes);
-        Console.WriteLine($"Given Rectangle Area is: {area.Area()[0]}cm^3 and \nGiven Circle area is: {area.Area()[1]}cm^3");
+        Console.WriteLine($"Given Rectangle Area is: {area.Area()[0]}cm^2 and \nGiven Circle area is: {area.Area()[1]}cm^2");
     }
 }
 
